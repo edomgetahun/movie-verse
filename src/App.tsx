1073e40faceb9d5c575b197f0c98d6e09e5f9bd7
Edom Getahun <edom.getahun@aastustudent.edu.et>
2026-07-30 11:48:55 +0300

    Refactor global layout and styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
+import { AnimatePresence, motion } from 'motion/react';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import GenrePage from './pages/GenrePage';
@@ -9,7 +10,70 @@ import MovieModal from './components/MovieModal';
 import { DownloadsProvider } from './context/DownloadsContext';
 import { FavoritesProvider } from './context/FavoritesContext';
 import { MovieModalProvider } from './context/MovieModalContext';
-import './App.css';
+import { EASE_OUT } from './lib/motion';
+
+function AnimatedRoutes() {
+  const location = useLocation();
+  return (
+    <AnimatePresence mode="wait">
+      <Routes location={location} key={location.pathname}>
+        <Route
+          path="/"
+          element={
+            <PageTransition>
+              <Home />
+            </PageTransition>
+          }
+        />
+        <Route
+          path="/genre/:slug"
+          element={
+            <PageTransition>
+              <GenrePage />
+            </PageTransition>
+          }
+        />
+        <Route
+          path="/search"
+          element={
+            <PageTransition>
+              <SearchResults />
+            </PageTransition>
+          }
+        />
+        <Route
+          path="/downloads"
+          element={
+            <PageTransition>
+              <Downloads />
+            </PageTransition>
+          }
+        />
+        <Route
+          path="/favorites"
+          element={
+            <PageTransition>
+              <Favorites />
+            </PageTransition>
+          }
+        />
+      </Routes>
+    </AnimatePresence>
+  );
+}
+
+function PageTransition({ children }: { children: React.ReactNode }) {
+  return (
+    <motion.div
+      initial={{ opacity: 0, y: 16 }}
+      animate={{ opacity: 1, y: 0 }}
+      exit={{ opacity: 0, y: -16 }}
+      transition={{ duration: 0.35, ease: EASE_OUT }}
+    >
+      {children}
+    </motion.div>
+  );
+}
 
 function App() {
   return (
@@ -18,14 +82,8 @@ function App() {
         <MovieModalProvider>
           <BrowserRouter>
             <Navbar />
-            <main className="page-container">
-              <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/genre/:slug" element={<GenrePage />} />
-                <Route path="/search" element={<SearchResults />} />
-                <Route path="/downloads" element={<Downloads />} />
-                <Route path="/favorites" element={<Favorites />} />
-              </Routes>
+            <main className="max-w-[1400px] mx-auto px-4 md:px-8 pb-16">
+              <AnimatedRoutes />
             </main>
             <MovieModal />
           </BrowserRouter>
